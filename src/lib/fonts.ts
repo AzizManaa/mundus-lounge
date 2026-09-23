09e29b1de984ea282cee1498eeb84b3b1b096627
AzizManaa <m.azizmanaa@gmail.com>
2026-09-24 01:03:53 +0200

    Compress local fonts for faster page loads

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,14 +1,14 @@
 import localFont from "next/font/local";
 
 export const bricolage = localFont({
-  src: "../app/fonts/bricolage/BricolageGrotesque-Variable.ttf",
+  src: "../app/fonts/bricolage/BricolageGrotesque-Variable.woff2",
   display: "swap",
   variable: "--font-bricolage",
   weight: "200 800",
 });
 
 export const timberline = localFont({
-  src: "../app/fonts/timberline/Timberline-Regular.otf",
+  src: "../app/fonts/timberline/Timberline-Regular.woff2",
   display: "swap",
   variable: "--font-timberline",
 });
